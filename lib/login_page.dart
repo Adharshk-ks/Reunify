5d071523911c08c4f18d2e6319efe5bbcbe3ba1f
Adharshk-ks <adharshks333@gmail.com>
2024-02-26 14:23:01 +0530

    background image added

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,69 +1,110 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-            title: const Text(
-              "Reunify",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
+          title: const Text(
+            "Reunify",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
-            backgroundColor: Colors.black),
-        body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 200,
-                child: Center(
-                  child: Text(
-                    "Log In",
-                    style: TextStyle(fontSize: 26),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 300,
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: Stack(
+          children: [
+            Image.asset(
+              'assets/login_bg1.png',
+              fit: BoxFit.fitHeight,
+              height: double.infinity,
+              opacity: const AlwaysStoppedAnimation(.75),
+            ),
+            Container(
+              // decoration: const BoxDecoration(color: Color.fromARGB(255, 127, 244, 95)),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    const TextField(
-                      decoration: InputDecoration(
-                        hintText: "Email",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(40),
-                          ),
+                    const SizedBox(
+                      height: 150,
+                      child: Center(
+                        child: Text(
+                          "Log In",
+                          style: TextStyle(fontSize: 26),
                         ),
                       ),
                     ),
-                    const TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Password",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(40),
+                    SizedBox(
+                      height: 250,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                              color: Colors.white,
+                            ),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                hintText: "Email",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(40),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),                       
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                              color: Colors.white,
+                            ),
+                            child: const TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                hintText: "Password",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(40),
+                                  ),
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text("Login"),
+                          ),
+                          RichText(
+                            text: const TextSpan(
+                              text: "Already have an account? ",
+                              style: TextStyle(fontSize: 16),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Sign in',
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text("login"),
-                    )
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
