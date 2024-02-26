962fea0da797ad54c78973562f354cb82c56e59d
Adharshk-ks <adharshks333@gmail.com>
2024-02-26 23:20:20 +0530

    sign up completed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool signUp = false;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,88 +26,176 @@ class LoginPage extends StatelessWidget {
         body: Stack(
           children: [
             Image.asset(
-              'assets/login_bg1.png',
+              signUp ? 'assets/pg_2.jpg' : 'assets/login_bg1.png',
               fit: BoxFit.fitHeight,
               height: double.infinity,
               opacity: const AlwaysStoppedAnimation(.75),
             ),
-            Container(
-              // decoration: const BoxDecoration(color: Color.fromARGB(255, 127, 244, 95)),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 150,
-                      child: Center(
-                        child: Text(
-                          "Log In",
-                          style: TextStyle(fontSize: 26),
-                        ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: ListView(
+                children: [
+                  SizedBox(
+                    height: 150,
+                    child: Center(
+                      child: Text(
+                        signUp ? "Sign Up" : "Log In",
+                        style: const TextStyle(fontSize: 26),
                       ),
                     ),
-                    SizedBox(
-                      height: 250,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(40),
-                              color: Colors.white,
-                            ),
-                            child: const TextField(
-                              decoration: InputDecoration(
-                                hintText: "Email",
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(40),
+                  ),
+                  SizedBox(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        signUp
+                            ? Container(
+                                margin: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  color: Colors.white,
+                                ),
+                                child: const TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "First Name",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(40),
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                          ),                       
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(40),
-                              color: Colors.white,
-                            ),
-                            child: const TextField(
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                hintText: "Password",
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(40),
+                              )
+                            : Container(),
+                        signUp
+                            ? Container(
+                                margin: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  color: Colors.white,
+                                ),
+                                child: const TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Last Name",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(40),
+                                      ),
+                                    ),
                                   ),
+                                ),
+                              )
+                            : Container(),
+                        signUp
+                            ? Container(
+                                margin: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  color: Colors.white,
+                                ),
+                                child: const TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Username",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(40),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            : Container(),
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            color: Colors.white,
+                          ),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              hintText: "Email",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(40),
                                 ),
                               ),
                             ),
                           ),
-                          ElevatedButton(
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            color: Colors.white,
+                          ),
+                          child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: signUp ? "Create Password": "Password",
+                              border: const OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(40),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        signUp
+                            ? Container(
+                                margin: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  color: Colors.white,
+                                ),
+                                child: const TextField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    hintText: "Confirm Password",
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(40),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            : Container(),
+                        Padding(
+                          padding: const EdgeInsets.only(top:19),
+                          child: ElevatedButton(
                             onPressed: () {},
-                            child: const Text("Login"),
+                            child: Text(signUp ? "SignUp" : "Login"),
                           ),
-                          RichText(
-                            text: const TextSpan(
-                              text: "Already have an account? ",
-                              style: TextStyle(fontSize: 16),
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text: 'Sign in',
-                                  style: TextStyle(
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                signUp
+                                    ? "Already have an account?"
+                                    : "Don't have an account? ",
+                                style: const TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    signUp = !signUp;
+                                  });
+                                },
+                                child: Text(
+                                  signUp ? " Login" : 'Sign up',
+                                  style: const TextStyle(
                                     color: Colors.blue,
                                     fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+                              ),
+                            ]),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
